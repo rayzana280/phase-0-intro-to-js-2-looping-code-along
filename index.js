9654c54486bde678b08df6b6f73517422d7033fa
Ray Zana <rayzana280@gmail.com>
2022-12-28 15:15:13 -0500

    commit tree bf7b9e84f569501545aca6adc6a2020306c23092

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    const newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+}
+
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+        number--
+    }
+}
